5325322ddf4188435a23f672866d55b975d11146
Anjjie <Anjjie@LAPTOP-SO7JVAVT>
2017-10-31 14:55:13 +0800

    测试

---
+++ eChengKJ_Web/我的类.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eChengKJ_Web
+{
+    public class 我的类
+    {
+        public string name { get; set; }
+    }
+}
